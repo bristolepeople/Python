88619bbf5bc52b5c01326f977f855ad5c07b8baa
davep <dave.patterson.bristol@gmail.com>
2021-05-23 21:45:19 +0100

    Improved wall drawing and corrected the hunt algorythm.

--- maze/maze_game.py
+++ maze/maze_game.py
@@ -5,8 +5,10 @@ import time
 pygame.init()
 
 # Setting the width and height of the screen [width, height]
-size = (800, 800)
+size = (1200, 700)
 screen = pygame.display.set_mode(size)
+cellSize = 20
+wallThickness = 2
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -20,8 +22,8 @@ class Cell(object):
         self._xLoc = x
         self._yLoc = y
         w, h = pygame.display.get_surface().get_size()
-        self._width = 50
-        self._height = self._width
+        self._width = cellSize
+        self._height = cellSize
         self._visited = False
         self._north = True
         self._west = True
@@ -33,25 +35,25 @@ class Cell(object):
         """
             draws the cell on the screen.
         """
-        pygame.draw.rect(screen, self._colour, (self._xLoc + 5, self._yLoc + 5, self._width -5 , self._height - 5), 0)
+        pygame.draw.rect(screen, self._colour, (self._xLoc + (wallThickness/2), self._yLoc + (wallThickness/2), self._width -(wallThickness/2) , self._height - (wallThickness/2)), 0)
 
     def drawWalls(self):
         if self._north:
-            pygame.draw.rect(screen, BLACK, (self._xLoc, self._yLoc, self._width, 5), 0)
+            pygame.draw.rect(screen, BLACK, (self._xLoc , self._yLoc, self._width , wallThickness), 0)
         else:
-            pygame.draw.rect(screen, WHITE, (self._xLoc, self._yLoc, self._width, 5), 0)
+            pygame.draw.rect(screen, WHITE, (self._xLoc + wallThickness , self._yLoc, self._width -wallThickness , wallThickness), 0)
         if self._south:
-            pygame.draw.rect(screen, BLACK, (self._xLoc, self._yLoc + self._height - 5 , self._width, 5), 0)
+            pygame.draw.rect(screen, BLACK, (self._xLoc  , self._yLoc + self._height  , self._width , wallThickness), 0)
         else:
-            pygame.draw.rect(screen, WHITE, (self._xLoc, self._yLoc + self._height - 5 , self._width, 5), 0)
+            pygame.draw.rect(screen, WHITE, (self._xLoc + wallThickness, self._yLoc + self._height  , self._width - wallThickness, wallThickness), 0)
         if self._west:
-            pygame.draw.rect(screen, BLACK, (self._xLoc, self._yLoc, 5, self._height), 0)
+            pygame.draw.rect(screen, BLACK, (self._xLoc, self._yLoc , wallThickness, self._height  ), 0)
         else:
-            pygame.draw.rect(screen, WHITE, (self._xLoc, self._yLoc, 5, self._height), 0)
+            pygame.draw.rect(screen, WHITE, (self._xLoc, self._yLoc + wallThickness , wallThickness, self._height - wallThickness), 0)
         if self._east:
-            pygame.draw.rect(screen, BLACK, (self._xLoc + self._width -5, self._yLoc, 5, self._height), 0)
+            pygame.draw.rect(screen, BLACK, (self._xLoc + self._width , self._yLoc , wallThickness, self._height ), 0)
         else:
-            pygame.draw.rect(screen, WHITE, (self._xLoc + self._width -5, self._yLoc, 5, self._height), 0)
+            pygame.draw.rect(screen, WHITE, (self._xLoc + self._width , self._yLoc  + wallThickness , wallThickness, self._height  -wallThickness ), 0)
 
 def findUnvisited(grid):
     """
@@ -64,12 +66,39 @@ def findUnvisited(grid):
                 unvisited.append((x,y))
     return unvisited
 
+def findFirstUnvisitedWithVisitedNeighbour(grid):
+    rightToLeft = bool(random.getrandbits(1))
+    bottomToTop = bool(random.getrandbits(1))
+    
+    xRange = range(len(grid))
+    if rightToLeft:
+        xRange = reversed(xRange)
+    yRange = range(len(grid[0]))
+#    if bottomToTop:
+#        yRange = reversed(yRange)
+    for x in xRange:
+        for y in yRange:
+            if grid[x][y]._visited:
+                unvisitedNeighbours = findUnvisitedNeighbours((x,y), grid)
+                if unvisitedNeighbours:
+                    return ((x,y))
+
+def findUnvisitedNeighbours(cell,grid):
+    neighbours = [(cell[0]+1,cell[1]),(cell[0]-1,cell[1]),(cell[0],cell[1]+1),(cell[0],cell[1]-1)]
+    myUnvisitedNeighbours = list(set(neighbours) & set(findUnvisited(grid)))
+    return myUnvisitedNeighbours
 
 def startHunt(grid):
     # Find random unvisited cell.
     unvisited = findUnvisited(grid)
     first = random.choice(unvisited)
     return hunt(grid,first,unvisited)
+
+def continueHunt(grid):
+    # Find random unvisited cell.
+    cell = findFirstUnvisitedWithVisitedNeighbour(grid)
+    unvisited = findUnvisited(grid)
+    return hunt(grid,cell,unvisited)
 
 def hunt(grid,cell,unvisited):
     
@@ -80,7 +109,7 @@ def hunt(grid,cell,unvisited):
     
     pressed = False
     
-    unvisited.remove(cell)
+    if cell in unvisited: unvisited.remove(cell)
 
     neighbours = [(cell[0]+1,cell[1]),(cell[0]-1,cell[1]),(cell[0],cell[1]+1),(cell[0],cell[1]-1)]
 
@@ -104,7 +133,7 @@ def hunt(grid,cell,unvisited):
         grid[next[0]][next[1]].drawWalls()
 
         pygame.display.flip()
-        time.sleep(0.1)
+        #time.sleep(0.01)
         hunt(grid,next,unvisited)  
 
     return grid
@@ -113,14 +142,14 @@ screen.fill(BLUE)
 
 grid = []
 
-size = 10
 
-for x in range(size):
+
+for y in range(30):
 
     row = []
 
-    for i in range(size):
-        row.append(Cell(screen, 100+i*50, 100 + x * 50))
+    for x in range(50):
+        row.append(Cell(screen, 10+x*cellSize, 10 + y * cellSize))
     for c in row:
         c.draw()
         c.drawWalls()
@@ -130,8 +159,9 @@ for x in range(size):
 
 pygame.display.flip()
 
+grid = startHunt(grid)
 while findUnvisited(grid):
-    grid = startHunt(grid)
+    grid = continueHunt(grid)
 
 pressed = False
 while not pressed:
